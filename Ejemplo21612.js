aa80b1b1f3fb09462eff9a19b052fa8fd14bb689
2013640173Ivan <rivanrfuentes@gmail.com>
2017-02-19 16:07:19 +0000

    Update Ejemplo21612.js

--- Ejemplo21612.js
+++ Ejemplo21612.js
@@ -1,4 +1,4 @@
-var miCanvas = document.getElementById("miCanvas1");
+var miCanvas1 = document.getElementById("miCanvas1");
 var contexto = miCanvas1.getContext("2d");
 contexto.fillStyle = "blue";
 contexto.fillRect(30, 30, 50, 50);
