ecc8225b1a2ce3d31c5d2b1a317cae644dbd168f
2013640173Ivan <rivanrfuentes@gmail.com>
2017-03-01 02:12:15 +0000

    Update unionmallas.js

--- unionmallas.js
+++ unionmallas.js
@@ -5,13 +5,13 @@ esferaForma.translate(0,1,0);
 var troncoMalla = new THREE.Mesh(troncoForma);
 var esferaMalla = new THREE.Mesh(esferaForma);
 
-var arbolForma = new Three.Geometry();
+var arbolForma = new THREE.Geometry();
 
 arbolForma.merge(troncoMalla.geometry, troncoMalla.matrix);
 arbolForma.merge(esferaMalla.geometry, esferaMalla.matrix);
 
 var material = new THREE.MeshNormalMaterial();
-var arbolMalla = new THREE.Mesh(arbolForma, material
+var arbolMalla = new THREE.Mesh(arbolForma, material);
 
 var escena = new THREE.Scene();
 escena.add(arbolMalla);
