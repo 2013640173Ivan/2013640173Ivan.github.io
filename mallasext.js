3f9b8fee5b49b70d4d0af9cd6ba5167b58d25fae
2013640173Ivan <rivanrfuentes@gmail.com>
2017-03-02 17:00:09 +0000

    Update mallasext.js

--- mallasext.js
+++ mallasext.js
@@ -34,13 +34,13 @@ var forma = new THREE.ExtrudeGeometry( figura,
                                        
                                 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
-//malla.rotateY( Math.PI/6 );
+malla.rotateY( Math.PI/4 );
 
 var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 1500;
+camara.position.z = 2000;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95,
