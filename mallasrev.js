2a8d83b73b4db334d0d75d09bea6576f944436ee
2013640173Ivan <rivanrfuentes@gmail.com>
2017-03-02 23:32:54 +0000

    Update mallasrev.js

--- mallasrev.js
+++ mallasrev.js
@@ -3,11 +3,28 @@ for ( var i = 0; i < 50; i ++ ) {
     puntos.push( new THREE.Vector2( Math.sin( i * 0.25 ) * 10 + 1.5, ( i - 5 ) * 3 ) );
 }
 
-var forma = new THREE.LatheGeometry(puntos);
+
+
+var x = 0, y = 0;
+
+var heartShape = new THREE.Shape();
+
+heartShape.moveTo( x + 5, y + 5 );
+heartShape.bezierCurveTo( x + 5, y + 5, x + 4, y, x, y );
+heartShape.bezierCurveTo( x - 6, y, x - 6, y + 7,x - 6, y + 7 );
+heartShape.bezierCurveTo( x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19 );
+heartShape.bezierCurveTo( x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7 );
+heartShape.bezierCurveTo( x + 16, y + 7, x + 16, y, x + 10, y );
+heartShape.bezierCurveTo( x + 7, y, x + 5, y + 5, x + 5, y + 5 );
+
+var geometry = new THREE.ShapeGeometry( heartShape );
+
+
+//var forma = new THREE.LatheGeometry(puntos);
 
 var material = new THREE.MeshNormalMaterial();
 
-var malla = new THREE.Mesh( forma, material );
+var malla = new THREE.Mesh( geometry, material );
 malla.rotateX( Math.PI/12 );
 
 var escena = new THREE.Scene();
