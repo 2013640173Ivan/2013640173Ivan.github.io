61b60bcd874ec51ff62abf4f79b14c7faad4012a
2013640173Ivan <rivanrfuentes@gmail.com>
2017-03-02 23:23:34 +0000

    Update mallasrev.js

--- mallasrev.js
+++ mallasrev.js
@@ -1,8 +1,8 @@
 var puntos = [];
 for ( var i = 0; i < 50; i ++ ) {
-    puntos.push( new THREE.Vector2(
+    points.push( new THREE.Vector2(
                      Math.sin( i * 0.3 ) * 15 + 50,
-                     ( i - 2.5 ) * 2 ) );
+                     ( i - 2.5 ) * 2 ) )points.push( new THREE.Vector2( Math.sin( i * 0.25 ) * 10 + 1.5, ( i - 5 ) * 3 ) );
 }
 
 var forma = new THREE.LatheGeometry(puntos);
