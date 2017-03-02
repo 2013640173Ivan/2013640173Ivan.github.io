e8b545590bd0df4ec3ed745b47f1e6e7db46ac1a
2013640173Ivan <rivanrfuentes@gmail.com>
2017-03-02 14:42:12 +0000

    Update unionmallas.js

--- unionmallas.js
+++ unionmallas.js
@@ -1,10 +1,10 @@
-var troncoForma = new THREE.CylinderGeometry(5, 5, 20, 32);
+var troncoForma = new THREE.CylinderGeometry(1, 1, 30, 64);
 var points = [];
 for ( var i = 0; i < 10; i ++ ) {
 	points.push( new THREE.Vector2( Math.sin( i * 0.2 ) * 10 + 5, ( i - 5 ) * 2 ) );
 }
-var esferaForma = new THREE.LatheGeometry( points );
-esferaForma.translate(1,3,1);
+var esferaForma = new THREE.LatheGeometry( points, 36 );
+esferaForma.translate(1,5,1);
 
 var troncoMalla = new THREE.Mesh(troncoForma);
 var esferaMalla = new THREE.Mesh(esferaForma);
@@ -33,7 +33,7 @@ var escena = new THREE.Scene();
 escena.add(florMalla, talloMalla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 50;
+camara.position.z = 150;
 
 renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
