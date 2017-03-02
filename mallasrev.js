2043cd9c8736a33bc4d013791f7426ff0b54cbba
2013640173Ivan <rivanrfuentes@gmail.com>
2017-03-02 05:11:25 +0000

    Update mallasrev.js

--- mallasrev.js
+++ mallasrev.js
@@ -16,7 +16,7 @@ var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.y = 150;
+camara.position.z = 1500;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
