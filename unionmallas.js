b84f5c436e5305f8e1441ee0dc4ce4f048ac4511
2013640173Ivan <rivanrfuentes@gmail.com>
2017-03-02 15:11:11 +0000

    Update unionmallas.js

--- unionmallas.js
+++ unionmallas.js
@@ -28,7 +28,7 @@ var escena = new THREE.Scene();
 escena.add(arbolMalla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 150;
+camara.position.z = 200;
 
 renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
