870adc5e16dcbc75539334fa0c28bf6938646382
2013640173Ivan <rivanrfuentes@gmail.com>
2017-03-02 16:55:46 +0000

    Update mallasext.js

--- mallasext.js
+++ mallasext.js
@@ -1,9 +1,33 @@
 var figura = new THREE.Shape();
 
-figura.moveTo(10, 10);
-figura.lineTo(10, 40);
-figura.lineTo(40, 40);
-figura.lineTo(10, 10);
+figura.moveTo(-160, -40);
+figura.lineTo(-185, -95);
+figura.lineTo(-280, -80);
+figura.lineTo(-240, 20);
+figura.lineTo(-165, 0);
+figura.lineTo(-170, 40);
+figura.lineTo(-150, 120);
+figura.lineTo(-400, 340);
+figura.lineTo(-260, 760);
+figura.lineTo(-70, 190);
+figura.lineTo(20, 220);
+figura.lineTo(135, 130);
+figura.lineTo(170, 80);
+figura.lineTo(520, 320);
+figura.lineTo(480, 60);
+figura.lineTo(190, 30);
+figura.lineTo(190, -60);
+figura.lineTo(130, -130);
+figura.lineTo(50, -150);
+figura.lineTo(140, -220);
+figura.lineTo(80, -280);
+figura.lineTo(0, -240);
+figura.lineTo(30, -160);
+figura.lineTo(-240, 20);
+figura.lineTo(-50, -170);
+figura.lineTo(-85, -95);
+figura.lineTo(-160, -40);
+figura.fill();
 
 var forma = new THREE.ExtrudeGeometry( figura,
                                        {amount: 10} );
