6dd3910c3795028ce69270781326a7e2f2be7c07
2013640173Ivan <rivanrfuentes@gmail.com>
2017-03-02 12:42:00 +0000

    Update unionmallas.js

--- unionmallas.js
+++ unionmallas.js
@@ -1,5 +1,5 @@
 var troncoForma = new THREE.CylinderGeometry(.25, .5, 1);
-var esferaForma = new THREE.TorusKnotBufferGeometry(10, 3, 100, 16 );
+var esferaForma = new THREE.TorusKnotBufferGeometry(10, 30, 64, 8);
 esferaForma.translate(1,1,1);
 
 var troncoMalla = new THREE.Mesh(troncoForma);
