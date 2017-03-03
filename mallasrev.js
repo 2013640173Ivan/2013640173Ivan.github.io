0d886d80f19faf284d9c0acaf43c18e0575c8077
2013640173Ivan <rivanrfuentes@gmail.com>
2017-03-03 03:53:05 +0000

    Update mallasrev.js

--- mallasrev.js
+++ mallasrev.js
@@ -4,7 +4,7 @@ var r1 = r0/2;
 var inc = Math.PI/r0;
 points = [];
 points.push( new THREE.Vector2( 0.0001, -r0/2 ) );
-for ( var i = 0; i <= r0/2; i ++ ) {
+for ( var i = 0; i <= r0; i ++ ) {
     var def = i<r0/2 ? 0 : r1*Math.sin((i-r0/2)*inc);
     points.push( new THREE.Vector2( Math.sqrt(r0*i-i*i), i+def-r0/2 ) );
 }
