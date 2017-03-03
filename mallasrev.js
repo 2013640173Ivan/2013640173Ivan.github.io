2fa8f83b0d92e6f675b5cf5dad3fbe6b099ef7d9
2013640173Ivan <rivanrfuentes@gmail.com>
2017-03-03 04:37:27 +0000

    Update mallasrev.js

--- mallasrev.js
+++ mallasrev.js
@@ -8,7 +8,7 @@ var sphere = new THREE.Mesh( geometry, material );
 
 var troncoForma = new THREE.CylinderGeometry(3, 6, 10);
 var troncoMalla = new THREE.Mesh(troncoForma);
-troncoMalla.position.set(0, -5, 0);
+troncoMalla.position.set(0, 0, 0);
 
 var ojoi=new THREE.SphereGeometry(1.5,32,32);
 ojoi.translate(-2,2,8);
