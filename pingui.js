52919f36a498c46c1d040c2df54532c367df6541
2013640173Ivan <rivanrfuentes@gmail.com>
2017-03-03 08:44:35 +0000

    Update pingui.js

--- pingui.js
+++ pingui.js
@@ -101,14 +101,14 @@ pingui.merge(mallapiei.geometry, mallapiei.matrix);
 pingui.merge(mallapied.geometry, mallapied.matrix);
 
 
-var mallapingui = new THREE.Mesh(pingui, material,mallapied,mallapiei);
+var mallapingui = new THREE.Mesh(pingui, material);
 //mallabomba.rotateY( Math.PI/4 );
 var escena = new THREE.Scene();
 //escena.add(mallapingui);
 
 
 //var escena = new THREE.Scene();
-escena.add(Ovo,sphere,mallojod,mallojoi,mallapic);
+escena.add(Ovo,sphere,mallojod,mallojoi,mallapic,mallapied,mallapie);
 
 
 var camara = new THREE.PerspectiveCamera();
