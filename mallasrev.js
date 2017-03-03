bc0ae5c5df512c527665a7a948d9b26218435f46
2013640173Ivan <rivanrfuentes@gmail.com>
2017-03-03 04:07:26 +0000

    Update mallasrev.js

--- mallasrev.js
+++ mallasrev.js
@@ -1,22 +1,32 @@
 
-var r0 = 20
-var r1 = r0/2;
-var inc = Math.PI/r0;
-points = [];
-points.push( new THREE.Vector2( 0.0001, -r0/2 ) );
-for ( var i = 0; i <= r0; i ++ ) {
-    var def = i<r0/2 ? 0 : r1*Math.sin((i-r0/2)*inc);
-    points.push( new THREE.Vector2( Math.sqrt(r0*i-i*i), i+def-r0/2 ) );
-}
-points.push( new THREE.Vector2( 0.0001, r0/2 ) );
+//var r0 = 20
+//var r1 = r0/2;
+//var inc = Math.PI/r0;
+//points = [];
+//points.push( new THREE.Vector2( 0.0001, -r0/2 ) );
+//for ( var i = 0; i <= r0; i ++ ) {
+  //  var def = i<r0/2 ? 0 : r1*Math.sin((i-r0/2)*inc);
+    //points.push( new THREE.Vector2( Math.sqrt(r0*i-i*i), i+def-r0/2 ) );
+//}
+//points.push( new THREE.Vector2( 0.0001, r0/2 ) );
 
-var cabeza = new THREE.LatheBufferGeometry( points, 32 );
+//var cabeza = new THREE.LatheBufferGeometry( points, 32 );
 
-var material = new THREE.MeshNormalMaterial();
+//var material = new THREE.MeshNormalMaterial();
 
-var mallacabeza = new THREE.Mesh( cabeza, material );
+//var mallacabeza = new THREE.Mesh( cabeza, material );
 //mallacabeza.rotateY( Math.PI/6 );
-mallacabeza.position.set(0, 10, 0);
+//mallacabeza.position.set(0, 10, 0);
+
+var geometry = new THREE.SphereGeometry( 8,16,16, Math.PI/2, Math.PI*2, 0, Math.PI);
+
+var material = new THREE.MeshBasicMaterial( { color: 0x2980b9} );
+material.side = THREE.BackSide;
+
+var sphere = new THREE.Mesh( geometry, material );
+
+//scene.add( sphere );
+
 
 var troncoForma = new THREE.CylinderGeometry(3, 6, 10);
 var troncoMalla = new THREE.Mesh(troncoForma);
@@ -28,7 +38,7 @@ var mallojoi =new THREE.Mesh( ojoi, material);
 
 var hongoForma = new THREE.Geometry();
 
-//hongoForma.merge(mallacabeza.geometry, mallacabeza.matrix);
+hongoForma.merge(sphere.geometry, sphere.matrix);
 hongoForma.merge(troncoMalla.geometry, troncoMalla.matrix);
 hongoForma.merge(mallojoi.geometry, mallojoi.matrix);
 
