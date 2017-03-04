09827adff0a00e6dbd5df81bfc3393657e5469cc
2013640173Ivan <rivanrfuentes@gmail.com>
2017-03-04 00:15:24 +0000

    Update pingui.js

--- pingui.js
+++ pingui.js
@@ -114,7 +114,7 @@ figurala.lineTo(4,0);
 
 
 var formala = new THREE.ExtrudeGeometry( figurala,
-                                       {amount: 2} );
+                                       {amount: .1} );
                                        
                                 var materiala = new THREE.MeshNormalMaterial();
 var mallala = new THREE.Mesh( formala, materiala );
