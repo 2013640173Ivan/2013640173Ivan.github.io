f7fe41cb441db5ecbf14fb39e56c2fdcfc8e045b
2013640173Ivan <rivanrfuentes@gmail.com>
2017-03-04 00:44:52 +0000

    Update hada.js

--- hada.js
+++ hada.js
@@ -1,3 +1,7 @@
 var geometry = new THREE.SphereGeometry( 50, 32, 32 );
 var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
 var sphere = new THREE.Mesh( geometry, material );
+
+var escena = new THREE.Scene();
+escena.add(sphere);
+
