b016644c8c4c2f2584ff78689faf06382196b9c9
2013640173Ivan <rivanrfuentes@gmail.com>
2017-03-04 01:51:35 +0000

    Update hada.js

--- hada.js
+++ hada.js
@@ -56,9 +56,27 @@ var mallaiu = new THREE.Mesh( formaiu, material );
 
 
 
+var figuraiue = new THREE.Shape();
+
+figuraiue.moveTo(-35,-45);
+figuraiue.lineTo(-25,-90);
+figuraiue.lineTo(-55,-75);
+figuraiue.lineTo(-75,-55);
+figuraiue.lineTo(-40,-40);
+figuraiue.lineTo(-35,-45);
+
+
+var formaiue = new THREE.ExtrudeGeometry( figuraiue,
+                                       {amount: 1} );
+                                       
+                                var material = new THREE.MeshNormalMaterial();
+var mallaiue = new THREE.Mesh( formaiue, material );
+
+
+
 
 var escena = new THREE.Scene();
-escena.add(sphere,malla,mallai,mallaiu);
+escena.add(sphere,malla,mallai,mallaiu,mallaiue );
 
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 500;
