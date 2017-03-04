aa7e8ab65015a370fc1d90453d95318516f6bfaa
2013640173Ivan <rivanrfuentes@gmail.com>
2017-03-04 00:47:04 +0000

    Update hada.js

--- hada.js
+++ hada.js
@@ -5,3 +5,10 @@ var sphere = new THREE.Mesh( geometry, material );
 var escena = new THREE.Scene();
 escena.add(sphere);
 
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 500;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
