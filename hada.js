7e20a2d39cc3e53b193ed886aa5bc3877b3fd7b5
2013640173Ivan <rivanrfuentes@gmail.com>
2017-03-04 01:32:06 +0000

    Update hada.js

--- hada.js
+++ hada.js
@@ -2,8 +2,27 @@ var geometry = new THREE.SphereGeometry( 50, 32, 32 );
 var material = new THREE.MeshBasicMaterial( {color: 0x0AF9EE} );
 var sphere = new THREE.Mesh( geometry, material );
 
+
+var figura = new THREE.Shape();
+
+figura.moveTo(40,40);
+figura.lineTo(150,60);
+figura.lineTo(200,180);
+figura.lineTo(35,45);
+figura.lineTo(40,40);
+
+
+var forma = new THREE.ExtrudeGeometry( figura,
+                                       {amount: 10} );
+                                       
+                                var material = new THREE.MeshNormalMaterial();
+var malla = new THREE.Mesh( forma, material );
+//malla.rotateY( -Math.PI/4 );
+
+
+
 var escena = new THREE.Scene();
-escena.add(sphere);
+escena.add(sphere,malla);
 
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 300;
@@ -12,3 +31,5 @@ var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
 renderizador.render( escena, camara );
+
+
