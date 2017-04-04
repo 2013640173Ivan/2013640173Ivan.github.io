dd129cd9eff7f075722456df0135419b4598bce3
2013640173Ivan <rivanrfuentes@gmail.com>
2017-04-04 14:06:21 -0500

    Update ejem0404174.js

--- ejem0404174.js
+++ ejem0404174.js
@@ -1,3 +1,27 @@
 function setup(){
-
 var forma = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.PhongMaterial({color: '#00cc00'});
+  malla = new THREE.Mesh(forma, material);
+  
+  var luzPuntual = new THREE.PointLight(0xFFFFFF);
+  luzPuntual.position.x = 10;
+  luzPuntual.position.y = 10;
+  luzPuntual.position.z = 10;
+  
+  escena = new THREE.Scene();
+  escena.add(malla);
+  escena.add(luzPuntual);
+  
+  camara = new THREE.PerspectiveCamera();
+  camara.position.z = 5;
+  
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerHeight*.95, window.innerHeight*.95);
+  document.body.appendChild( renderer.documentElement );
+}
+
+function loop() {
+  
+  requestAnimationFrame( loop );
+  
+  
