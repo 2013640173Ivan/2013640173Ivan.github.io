e5238dcf7061e6527c1e578f1e7ca5665299a75a
2013640173Ivan <rivanrfuentes@gmail.com>
2017-04-04 13:50:58 -0500

    Update ejem0404173.js

--- ejem0404173.js
+++ ejem0404173.js
@@ -16,7 +16,7 @@ escena.add(malla);
   
 
 camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z = 755;
 
 renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerHeight*.95, window.innerHeight*.95);
