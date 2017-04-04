969d1737f3ca4aa47bb8acc70913832a9a53df7d
2013640173Ivan <rivanrfuentes@gmail.com>
2017-04-04 14:06:56 -0500

    Update ejem0404174.js

--- ejem0404174.js
+++ ejem0404174.js
@@ -24,4 +24,15 @@ function loop() {
   
   requestAnimationFrame( loop );
   
+  malla.rotation.x += 0.01;
+  malla.rotation.y += 0.01;
+
+renderer.render( escena, camara );
+}
+
+var camara, escena, renderer, malla;
+
+setup();
+loop();
+  
   
