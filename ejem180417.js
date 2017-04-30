cf68bb377cd376caf65d0d25ba12272ed082eb68
2013640173Ivan <rivanrfuentes@gmail.com>
2017-04-30 20:59:49 +0100

    Update ejem180417.js

--- ejem180417.js
+++ ejem180417.js
@@ -13,8 +13,8 @@ camara.position.z=5;
 
 var lienzo = document.getElementById("luzAmbiental");
 var renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
+renderizador.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderizador.domElement);
-renderizador.setSize(600,600);
 renderizador.render(escena, camara);
 
 
