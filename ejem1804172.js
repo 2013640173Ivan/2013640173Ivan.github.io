76697e81c6d1fdc66eaf6a46ae846f9eb62cb93d
2013640173Ivan <rivanrfuentes@gmail.com>
2017-05-01 00:24:55 +0100

    Update ejem1804172.js

--- ejem1804172.js
+++ ejem1804172.js
@@ -1,7 +1,10 @@
 var iluminacion = new THREE.Ambientallight(0xFFFFFF)
-var iluminacion = new THREE.SphereGeometry(1);
+iluminacion.position.y=20;
+
+var forma = new THREE.SphereGeometry(1);
 var material = new THREE.MeshLambertMaterial({color: "#00c00"});
 var malla = new THREE.Mesh( forma, material);
+
 malla.position.y=2;
 
 var base = new THREE.Mesh(new THREE.BoxGeometry(5,.1,5), new THREE.MeshLambertMaterial({color: 0XFFFFFF}));
@@ -15,9 +18,14 @@ var camara = new THREE.PerspectiveCameraq();
 camara.position.z=15;
 camara.position.y=5;
 
-var lienzo = document.getElementbyId("luzSinSombras");
-var renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
 
-renderizador.setSize(600, 600);
-renderizador.renderer(escena, camara);
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild( renderizador.domElement );
 
+
+renderizador.shadowMapEnabled = true;
+malla.castShadow = true;
+base.receiveShadow = true;
+iluminacion.castShadow = true;
+renderizador.render(escena, camara);
