41eac275b71c89d2b7884f2ea29cc2e155d8bc77
2013640173Ivan <rivanrfuentes@gmail.com>
2017-05-01 06:11:15 +0100

    Update ejem1804172.js

--- ejem1804172.js
+++ ejem1804172.js
@@ -3,12 +3,12 @@ iluminacion.position.y=20;
 iluminacion.position.x=20;
 
 var forma = new THREE.SphereGeometry(1);
-var material = new THREE.MeshPhongMaterial({color: "#33F6FF"});
+var material = new THREE.MeshLambertMaterial({color: "#33F6FF"});
 var malla = new THREE.Mesh( forma, material);
 
 malla.position.y=2;
 
-var base = new THREE.Mesh(new THREE.BoxGeometry(5,.1,5), new THREE.MeshPhongMaterial({color: 0XFFFFFF}));
+var base = new THREE.Mesh(new THREE.BoxGeometry(5,.1,5), new THREE.MeshLambertMaterial({color: 0XFFFFFF}));
 
 var escena = new THREE.Scene();
 escena.add(malla);
