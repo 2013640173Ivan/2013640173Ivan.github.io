eb42afa7dc1a6801aea89c9a53d4007743a17936
2013640173Ivan <rivanrfuentes@gmail.com>
2017-05-09 13:47:40 -0500

    Update textura2.js

--- textura2.js
+++ textura2.js
@@ -1,16 +1,24 @@
 function setup (){
 THREE.ImageUtils.crossOrigin = '';
 var textura =THREE.ImageUtils.loadTexture('2013640173Ivan.github.io/brick_diffuse.jpg');
-var material = new THREE.MeshBasicMaterial({map: textura});
+var material = new THREE.MeshLambertMaterial({map: textura});
 var forma = new THREE.BoxGeometry(1,1,1);
 malla = new THREE.Mesh(forma, material);
+  
+  var luzPuntual = new THREE.PointLight(0xFFFFFF);
+  luzPuntual.position.x=10;
+  luzPuntual.position.y=10;
+  luzPuntual.position.z=10;
+  
+  
 
 
 escena = new THREE.Scene();
 escena.add(malla);
+  escena.add(luzPuntual);
 
 camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z = 10;
 
 renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*.95, window.innerHeight*.95);
