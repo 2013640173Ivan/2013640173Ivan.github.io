24d5f4a99ca43956f1d5cdbaed3caa7cdb27eb91
2013640173Ivan <rivanrfuentes@gmail.com>
2017-05-23 13:36:54 -0500

    Update colisiones.js

--- colisiones.js
+++ colisiones.js
@@ -1,8 +1,8 @@
 function setup () {
 
-cubo1 = new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new
+cubo1 = new THREE.Mesh(new THREE.BoxGeometry(1,1,1),
                         new THREE.MeshNormalMaterial());
-cubo2 = new THREE.Mesh ( new THREE.BoxGeometry(1,1,1),new
+cubo2 = new THREE.Mesh ( new THREE.BoxGeometry(1,1,1),
                         new THREE.MeshNormalMaterial());
                         
     cubo1.position.x = 0.7;
