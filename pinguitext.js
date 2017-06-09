f2742abe9a8037e0e78d38e5a46cc51a93fcf9bb
2013640173Ivan <rivanrfuentes@gmail.com>
2017-06-09 14:32:12 +0100

    Create pinguitext.js

---
+++ pinguitext.js
@@ -0,0 +1,11 @@
+var points = [];
+for ( var deg = 0; deg <= 180; deg += 6 ) {
+    var rad = Math.PI * deg / 180;
+    var point = new THREE.Vector2( ( 10*0.72 + 10*.08 * Math.cos( rad ) ) * Math.sin( rad ), - 10*Math.cos( rad ) ); // the "egg equation"
+    //console.log( point ); // x-coord should be greater than zero to avoid degenerate triangles; it is not in this formula.
+    points.push( point );
+}
+
+geometry = new THREE.LatheGeometry( points, 32 );
+var Ovo = new THREE.Mesh( geometry,material);
+Ovo.position.set(0, 0, 0);
