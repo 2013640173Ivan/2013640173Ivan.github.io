b60f4876e48d1b54dc1504c59fd810c68a21c47d
2013640173Ivan <rivanrfuentes@gmail.com>
2017-06-10 23:11:49 +0100

    Update pingui.js

--- pingui.js
+++ pingui.js
@@ -136,7 +136,7 @@ renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
 renderizador.render( escena, camara );
 
-}
+
 
 function loop() 
 {
