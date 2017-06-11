0d6b5a66d5784d06b1ca6cf98019d81f75c997bd
2013640173Ivan <rivanrfuentes@gmail.com>
2017-06-11 22:25:06 +0100

    Create hada.js

--- hada.js
+++ hada.js
@@ -103,7 +103,7 @@ escena.add(mallahada);
 //escena.add(sphere,malla,mallai,mallaiu,mallaiue );
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 120;
+camara.position.z = 50;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
