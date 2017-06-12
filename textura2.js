285e683af334ab54320422f37d8a8e2cbc5d84cf
2013640173Ivan <rivanrfuentes@gmail.com>
2017-06-12 02:54:41 +0100

    Create textura2.js

--- textura2.js
+++ textura2.js
@@ -3,40 +3,16 @@ THREE.ImageUtils.crossOrigin = '';
 //var textura =THREE.ImageUtils.loadTexture('2013640173Ivan.github.io/easter.jpg');
   var textura =THREE.ImageUtils.loadTexture('crate.gif');
 var material = new THREE.MeshLambertMaterial({map: textura});
-  var material2 = new THREE.MeshLambertMaterial();
-  var points = [];
-for ( var deg = 0; deg <= 180; deg += 6 ) {
-    var rad = Math.PI * deg / 180;
-    var point = new THREE.Vector2( ( 10*0.72 + 10*.08 * Math.cos( rad ) ) * Math.sin( rad ), - 10*Math.cos( rad ) ); // the "egg equation"
-    //console.log( point ); // x-coord should be greater than zero to avoid degenerate triangles; it is not in this formula.
-    points.push( point );
-}
 
- var forma = new THREE.LatheGeometry( points, 32 );
-  
-//var forma = new THREE.BoxGeometry(1,1,1);
-malla = new THREE.Mesh(forma, material2);
-  
-  
-  
-  
-  
-var ojos=new THREE.SphereGeometry(5,32,32);
-ojos.translate(0,12,0);
- mallojos =new THREE.Mesh( ojos, material2);
-  
-  
-  
-  
-  
-  var pingui = new THREE.Geometry();
-pingui.merge(malla.geometry, malla.matrix);
-pingui.merge(mallojos.geometry, mallojos.matrix);
-  
-  var mallapingui = new THREE.Mesh(pingui, material);
-//mallapingui.rotateY( Math.PI/8 );
 
-  
+var geometry = new THREE.BoxGeometry( 28, 28, 28 );
+var cube = new THREE.Mesh( geometry, material );
+var escena = new THREE.Scene();
+scena.add( cube );
+
+
+
+ 
   
   
   var luzPuntual = new THREE.PointLight(0xFFFFFF);
@@ -47,8 +23,7 @@ pingui.merge(mallojos.geometry, mallojos.matrix);
   
 
 
-escena = new THREE.Scene();
-escena.add(mallapingui);
+
   
   escena.add(luzPuntual);
 
@@ -67,9 +42,9 @@ requestAnimationFrame(loop);
 
 
 
-mallapingui.rotation.x += 0.01;
+cube.rotation.x += 0.01;
 
-mallapingui.rotation.y += 0.01;
+cube.rotation.y += 0.01;
 
 
 renderer.render(escena, camara);
