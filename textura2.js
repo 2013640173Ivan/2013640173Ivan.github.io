810b3cc23315a8e39b192eac1252d9b40d6e0aeb
2013640173Ivan <rivanrfuentes@gmail.com>
2017-06-12 04:58:30 +0100

    Create textura2.js

--- textura2.js
+++ textura2.js
@@ -6,7 +6,7 @@ var material = new THREE.MeshLambertMaterial({map: textura});
 
 
 var geometry = new THREE.BoxGeometry( 10, 8, 7 );
-geometry.translate(0,0,0);
+geometry.translate(0,0,-7);
 var cube = new THREE.Mesh( geometry, material );
 
 var escena = new THREE.Scene();
