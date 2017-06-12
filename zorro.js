52e76632a8cd1f4d851b264d965ba25b87d38063
2013640173Ivan <rivanrfuentes@gmail.com>
2017-06-12 02:36:04 +0100

    Create zorro.js

---
+++ zorro.js
@@ -0,0 +1,36 @@
+// Creamos un prisma
+var geometriaCubo = new THREE.CubeGeometry(
+    100, // Dimensiones en eje X
+    140, // Dimensiones en eje Y
+    100 // Dimensiones en eje Z
+);
+  
+// Creamos una apariencia (de lila claro)
+var aparienciaLila = new THREE.MeshLambertMaterial({
+    color: 0x9999FF // Color hexadecimal
+});
+  
+// Generamos el prisma y le aplicamos la apariencia
+var cubo = new THREE.Mesh(geometriaCubo, aparienciaLila);
+
+
+
+
+var escena = new THREE.Scene()
+escena.add(cubo);
+//escena.add(mallabot2);
+
+
+
+
+
+//var escena = new THREE.Scene();
+//escena.add(sphere,malla,mallai,mallaiu,mallaiue );
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 50;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
