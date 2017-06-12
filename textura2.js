527a8173c8e12faa370f6b6cda6ec85278736926
2013640173Ivan <rivanrfuentes@gmail.com>
2017-06-12 06:06:08 +0100

    Create textura2.js

--- textura2.js
+++ textura2.js
@@ -29,12 +29,18 @@ var geometry5 = new THREE.BoxGeometry(3,5,1.5);
 geometry5.translate(2.5,10.5,6.75);
 var cube5 = new THREE.Mesh( geometry5, material2 );
 
+
+var geometry6 = new THREE.BoxGeometry(3,5,1.5);
+geometry6.translate(-2.5,10.5,6.75);
+var cube6 = new THREE.Mesh( geometry6, material2 );
+
 var zo = new THREE.Geometry();
 zo.merge(cube.geometry, cube.matrix);
 zo.merge(cube2.geometry, cube2.matrix);
 zo.merge(cube3.geometry, cube3.matrix);
 zo.merge(cube4.geometry, cube4.matrix);
 zo.merge(cube5.geometry, cube5.matrix);
+zo.merge(cube6.geometry, cube6.matrix);
 
 var mallazo = new THREE.Mesh(zo, material);
 //mallabomba.rotateY( Math.PI/4);
