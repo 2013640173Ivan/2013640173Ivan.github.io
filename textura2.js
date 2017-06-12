a9d231ccf5aa531802ca764a51cca7acf35f9d0c
2013640173Ivan <rivanrfuentes@gmail.com>
2017-06-12 05:22:42 +0100

    Create textura2.js

--- textura2.js
+++ textura2.js
@@ -9,10 +9,16 @@ var geometry = new THREE.BoxGeometry( 8, 7, 10 );
 geometry.translate(8,0,0);
 var cube = new THREE.Mesh( geometry, material );
 
+
+var geometry2 = new THREE.BoxGeometry(10,3,4);
+geometry2.translate(0,2,5);
+var cube2 = new THREE.Mesh( geometry2, material );
+
 var escena = new THREE.Scene();
 
 
 escena.add( cube );
+escena.add(ciben2);
 
 
 
@@ -46,9 +52,9 @@ requestAnimationFrame(loop);
 
 
 
-cube.rotation.x += 0.00;
+cube.rotation.x += 0.01;
 
-cube.rotation.y += 0.00;
+cube.rotation.y += 0.01;
 
 
 renderer.render(escena, camara);
